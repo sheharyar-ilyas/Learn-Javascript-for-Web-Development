597d40646d7155c607892eab02e6f65fd3d15bca
Muhammad Sheharyar <msheharyar1885@gmail.com>
2026-08-21 07:59:29 +0000

    learn Conversion between Datatypes

---
+++ 01_Basics_Of_Javascript/Conversion_Operation.js
@@ -0,0 +1,50 @@
+// let score = "33"
+// let SCORE = "33jsajkv"
+// let scoring = null
+// let scorings = undefined
+// let scoring_booleant = true
+// let scoring_booleanf = false
+
+// console.log(typeof score);
+// console.log(typeof SCORE);
+// console.log(typeof scoring);
+// console.log(typeof scorings);
+
+// let valueInNumber = Number(score);
+// let valueInNumbers = Number(SCORE);
+// let valueInNumbering = Number(scoring);
+// let valueInNumberings = Number(scorings);
+// let valueInNumbering_T = Number(scoring_booleant);
+// let valueInNumbering_F = Number(scoring_booleanf);
+
+// console.log(typeof valueInNumber) // output:number
+// console.log(valueInNumber) // output: 33
+
+// console.log(typeof valueInNumbers) // output:number
+// console.log(valueInNumbers) // output:NaN mean Not a Number, So be careful about converting datatypes
+
+// console.log(typeof valueInNumbering) // output:number
+// console.log(valueInNumbers) // output:NaN mean Not a Number, So be careful about converting datatypes
+
+// console.log(typeof valueInNumberings) // output:number
+// console.log(valueInNumberings) // output:NaN, So be careful about converting datatypes
+
+// console.log(typeof valueInNumbering_T) // output:number
+// console.log(valueInNumbering_T) // output:1, So be careful about converting datatypes
+
+// console.log(typeof valueInNumbering_F) // output:number
+// console.log(valueInNumbering_F) // output:0, So be careful about converting datatypes
+
+// let loggedIn = undefined
+// let con_to_boolean = Boolean(loggedIn)
+// console.log(typeof con_to_boolean);
+// console.log(con_to_boolean);
+
+// 1 or greater than 1 = true , 0=false
+// ""= false , "anything in between" = true
+// null , undefined = false
+
+let str = undefined
+let con_str = String(str)
+console.log(typeof con_str)
+console.log(con_str)
