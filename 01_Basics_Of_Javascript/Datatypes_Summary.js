378e6a309686c66e8cca3ff74394a39bdebc3b93
Muhammad Sheharyar <msheharyar1885@gmail.com>
2026-08-22 09:04:39 +0000

    Disscussion about stack and heap memory

--- 01_Basics_Of_Javascript/Datatypes_Summary.js
+++ 01_Basics_Of_Javascript/Datatypes_Summary.js
@@ -20,21 +20,52 @@
 
 // console.log(typeof BigNumber)
 
-let arr = ["Ali", "Sheharyar", "Ahmad"];
-let myObj = {
-    name : 'Sheharyar',
-    age : 19,
-    grade : 'A'
-}
+// let arr = ["Ali", "Sheharyar", "Ahmad"];
+// let myObj = {
+//     name : 'Sheharyar',
+//     age : 19,
+//     grade : 'A'
+// }
 
-const myfun = function(){
-    console.log("My name is Muhammad Sheharyar.");
-}
+// const myfun = function(){
+//     console.log("My name is Muhammad Sheharyar.");
+// }
 
-console.log(typeof arr);
-console.log(typeof myObj);
-console.log(typeof myfun);
+// console.log(typeof arr);
+// console.log(typeof myObj);
+// console.log(typeof myfun);
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 // We can also study these type of things in this given url
+
+
+// +++++++++++++ About Memory +++++++++++++++++
+
+// Stack ( Primitive datatypes ) ---> make copy
+// Heap ( Non-Primitive ) ---> Take reference of memory
+
+// let name = "Sheharyar"
+// let anothername = name
+
+// anothername = "ALi"
+
+// console.log(anothername)
+// console.log(name)
+// It uses stack memory to store data
+
+
+// let myObj = {
+//     name : 'Sheharyar',
+//     age : 19,
+//     grade : 'A'
+// }
+
+// let obj2 = myObj
+
+// obj2.age = 20
+
+// console.log(obj2.age)
+// console.log(myObj.age)
+
+// It uses heap memory to store reference
