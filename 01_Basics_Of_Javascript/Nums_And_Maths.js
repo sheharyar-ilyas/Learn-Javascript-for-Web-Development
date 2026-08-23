c088e64780d8181530a0458e319136968c74f657
Muhammad Sheharyar <msheharyar1885@gmail.com>
2026-08-23 07:52:51 +0000

    Add Nums_And_Maths.js with mathematical operations and examples

---
+++ 01_Basics_Of_Javascript/Nums_And_Maths.js
@@ -0,0 +1,46 @@
+// const num = 200.8990
+// console.log(num)
+// console.log(num.toFixed(2))
+// const num1 = new Number(400)
+// console.log(num1)
+
+// console.log(num1.toString())
+// console.log(typeof num1)
+
+// console.log(isFinite(0/0))
+
+// function eArabic(x) {
+//   return x.toLocaleString("ar-EG");
+// }
+// function eArabic(x) {
+//   return x.toLocaleString("en-US");
+// }
+
+// console.log(eArabic(123456.789));
+// // Expected output: "١٢٣٬٤٥٦٫٧٨٩"
+
+// console.log(eArabic("123456.789"));
+// // Expected output: "123456.789"
+
+// console.log(eArabic(NaN));
+// // Expected output: "ليس رقم"
+// let num2 = 346.98399
+// console.log(num2.toPrecision(3)) //range between 1 to 21 and it also return string 
+// const num3 = 100000000
+// console.log(num3.toLocaleString())
+// console.log(num3.toExponential())
+
+// ----------------------  Math ------------------------------
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.sqrt(25))
+// console.log(Math.floor(8.637))
+// console.log(Math.ceil(8.637))
+// console.log(Math.random()) // It generates value between 0 and 1
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min))  // Important line.....
+
