406bbcf655c09a39b5615c8e504bd31a1bb8e53b
Muhammad Sheharyar <msheharyar1885@gmail.com>
2026-09-15 13:17:15 +0000

    scopes of variable complete

---
+++ 03_basics/scopes.js
@@ -0,0 +1,51 @@
+// if(true)
+// {
+//     const a =40
+//     let b = 90
+//     var c = 900
+// }
+
+// console.log(a)  // we get error
+// console.log(b)  // we ger error
+// console.log(c)  // but in this we not get error 
+// so result is that var ignores the block scopes which causes the bugs in the javascript code if same varaiable use
+// const a = 50
+
+// if(true)
+// {
+//     const a =40
+//     console.log("inner value",a)
+//     let b = 90
+//     var c = 900
+// }
+
+// console.log(a)
+
+// function one()
+// {
+//     const username = "sheharyar"
+
+//     function two()
+//     {
+//         const website = "discord"
+//         console.log(`${username} sees this  ${website} frequently.` )
+//     }
+
+//     two()
+// }
+
+// one()
+// console.log(addone(6))
+
+// function addone(num)
+// {
+//      return num +1
+// }
+
+// const addtwo = function addone(num)
+// {
+//      return num +1
+// }
+
+// console.log(addtwo(9))
+
