54a75e2f4abde9041703dc695577d875adcd9f90
Muhammad Sheharyar <msheharyar1885@gmail.com>
2026-09-18 14:42:32 +0000

    Loops with break and continue statement completes (for, while, do-while loop)

---
+++ 05_Loops/forloop.js
@@ -0,0 +1,18 @@
+// for(let index = 0; index <=10; index ++)
+// {
+//     console.log("Sheharyar")
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Table of number ${i} is as follow :`)
+//     for (let j = 1; j <=10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     } 
+// }
+
+const array = [0,1,2,3,4,5,3,5,3,5]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+}
---
+++ 05_Loops/while.js
@@ -0,0 +1,15 @@
+const array = ["sheharyar", "asfandyar", "ahsanyar"]
+
+let arr = 0
+while (arr < array.length) {
+        console.log(array[arr])
+        arr = arr +1
+}
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <=10);
